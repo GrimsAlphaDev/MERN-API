90efacceebe9e4ae86db9947954757fafaaf032e
GrimsAlphaDev <kungkingkangcanyouseemekuking@gmail.com>
2023-11-01 10:17:57 +0700

    update testing api

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser');
 
-app.use(() => {
-    console.log('SERVER IS RUNNING');
+const app = express();
+const productRoutes = require('./src/routes/products');
+
+app.use(bodyParser.json()); // Type Json
+
+app.use((req,res,next) => {
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
+    res.setHeader('Access-Control-Allow-Headers','Content-Type, Authorization');
+    next();
 });
+
+app.use('/v1/customer', productRoutes);
 
 app.listen(4000);
